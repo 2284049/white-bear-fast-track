00e0c7f169c586b40719d3bdf4e7f4d9099ca504
2284049 <70497897+2284049@users.noreply.github.com>
2020-09-03 18:18:52 -0700

    add edit page

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $("#save-card").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
 });
+
+$("#show-delete").click(function () {
+   $("#delete-button").removeClass("d-none");
+});
