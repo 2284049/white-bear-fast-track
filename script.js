4abb479c42170201a49eaaf2ca75d9e4b0ca7dd9
2284049 <70497897+2284049@users.noreply.github.com>
2020-09-04 19:29:58 -0700

    add landing page

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ $("#save-card").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
 });
 
+$("#back-to-answer").click(function () {
+   $("#overlay-error").toggleClass("d-flex d-none");
+});
+
 $("#show-delete").click(function () {
    $("#delete-button").removeClass("d-none");
 });
