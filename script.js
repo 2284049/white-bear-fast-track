a003e4a36994e45b8601690f1d0470f61d43ad5c
2284049 <70497897+2284049@users.noreply.github.com>
2020-09-16 13:10:11 -0700

    add character counters and sign up errors

--- script.js
+++ script.js
@@ -2,39 +2,130 @@ $("#save-card").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
 });
 
-let imageryCharsCount = 0; // Put it above the function, so that it's only zero once, not everytime they keypress again.
+$("#create-imagery-input").keyup(function (e) {
+   console.log("Event: ", e);
 
-$("#create-imagery-input").keydown(function (e) {
-   const key = e.which;
-   console.log("Key inputted: ", key);
+   // get the text frrom the textarea
+   const text = e.target.value;
+   // console.log("The user inputted: ", text);
+   console.log(`The user inputted: ${text}`);
 
-   // If the user presses backspace, decrement the count
-   // key 8 is backspace. Every key on the keyboard has a number assigned to it.
-   if (key === 8) {
-      console.log("The user has pressed backspace!");
-      imageryCharsCount -= 1;
-      if (imageryCharsCount < 0) {
-         console.log("You have entered negative territory!");
-         imageryCharsCount = 0;
-      }
-   } else if (key === 16 || 18 || 46 || 35 || 36 || 20) {
-      // key 16 is the shift key; two pipes is "OR"
-      console.log("This doesn't count in the character counter.");
-   } else {
-      //Else, increment the count
-      console.log("The user has pressed any other key!");
-      imageryCharsCount += 1;
-      /*
-   Be careful with decimals in JavaScript, "floating point"
-
-   Could use:
-   imageryCharsCount = imageryCharsCount + 1;
-   imageryCharsCount++   (this adds one increment each time, can also do --= to subtract an increment each time)
-   */
+   // check the length of the text
+   const textLength = text.length;
+   console.log(`The length of the text is: ${textLength}`);
+   if (textLength < 241 && textLength > 0) {
+      $("#save-card").removeAttr("disabled");
+      $("#imagery-characters").removeClass("text-danger");
+      $("#imagery-characters").addClass("text-muted");
+   } else if (textLength > 240) {
+      $("#save-card").attr("disabled", "disabled");
+      $("#imagery-characters").removeClass("text-muted");
+      $("#imagery-characters").addClass("text-danger");
+   } else if (textLength === 0) {
+      $("#save-card").attr("disabled", "disabled");
+      $("#imagery-characters").removeClass("text-danger");
+      $("#imagery-characters").addClass("text-muted");
    }
 
-   console.log("Total inputted chars: ", imageryCharsCount);
-   $("#imagery-char-count").html(imageryCharsCount);
+   // update the character counter
+   $("#imagery-char-count").html(textLength);
+});
+
+$("#create-answer-input").keyup(function (e) {
+   console.log("Event: ", e);
+
+   // get the text frrom the textarea
+   const text = e.target.value;
+   // console.log("The user inputted: ", text);
+   console.log(`The user inputted: ${text}`);
+
+   // check the length of the text
+   const textLength = text.length;
+   console.log(`The length of the text is: ${textLength}`);
+   if (textLength < 241 && textLength > 0) {
+      $("#next").removeAttr("disabled");
+      $("#answer-characters").removeClass("text-danger");
+      $("#answer-characters").addClass("text-muted");
+   } else if (textLength > 240) {
+      $("#next").attr("disabled", "disabled");
+      $("#answer-characters").removeClass("text-muted");
+      $("#answer-characters").addClass("text-danger");
+   } else if (textLength === 0) {
+      $("#next").attr("disabled", "disabled");
+      $("#answer-characters").removeClass("text-danger");
+      $("#answer-characters").addClass("text-muted");
+   }
+
+   // update the character counter
+   $("#answer-char-count").html(textLength);
+});
+
+$("#edit-imagery-input").keyup(function (e) {
+   console.log("Event: ", e);
+   const text = e.target.value;
+   console.log(`The user inputted: ${text}`);
+   const textLength = text.length;
+   console.log(`The length of the text is: ${textLength}`);
+   if (textLength < 241 && textLength > 0) {
+      $("#edit-imagery-characters").removeClass("text-danger");
+   } else if (textLength > 240) {
+      $("#edit-imagery-characters").addClass("text-danger");
+   } else if (textLength === 0) {
+      $("#edit-imagery-characters").removeClass("text-danger");
+   }
+   const imageryInput = $("#edit-imagery-input").val();
+   console.log(`The user inputted: ${imageryInput}`);
+   const imageryInputLength = imageryInput.length;
+   console.log(`The length of the imagery is: ${imageryInputLength}`);
+   const answerInput = $("#edit-answer-input").val();
+   console.log(`The user inputted: ${answerInput}`);
+   const answerInputLength = answerInput.length;
+   console.log(`The length of the answer is: ${answerInputLength}`);
+
+   if (
+      imageryInputLength > 0 &&
+      imageryInputLength < 241 &&
+      answerInputLength > 0 &&
+      answerInputLength < 241
+   ) {
+      $("#edit-save").removeAttr("disabled");
+   }
+   // update the character counter
+   $("#edit-imagery-char-count").html(textLength);
+});
+
+$("#edit-answer-input").keyup(function (e) {
+   console.log("Event: ", e);
+   const text = e.target.value;
+   console.log(`The user inputted: ${text}`);
+   const textLength = text.length;
+   console.log(`The length of the text is: ${textLength}`);
+   if (textLength < 241 && textLength > 0) {
+      $("#edit-answer-characters").removeClass("text-danger");
+   } else if (textLength > 240) {
+      $("#edit-answer-characters").addClass("text-danger");
+   } else if (textLength === 0) {
+      $("#edit-answer-characters").removeClass("text-danger");
+   }
+   const imageryInput = $("#edit-imagery-input").val();
+   console.log(`The user inputted: ${imageryInput}`);
+   const imageryInputLength = imageryInput.length;
+   console.log(`The length of the imagery is: ${imageryInputLength}`);
+   const answerInput = $("#edit-answer-input").val();
+   console.log(`The user inputted: ${answerInput}`);
+   const answerInputLength = answerInput.length;
+   console.log(`The length of the answer is: ${answerInputLength}`);
+
+   if (
+      imageryInputLength > 0 &&
+      imageryInputLength < 241 &&
+      answerInputLength > 0 &&
+      answerInputLength < 241
+   ) {
+      $("#edit-save").removeAttr("disabled");
+   }
+   // update the character counter
+   $("#edit-answer-char-count").html(textLength);
 });
 
 $("#back-to-answer").click(function () {
@@ -46,6 +137,32 @@ $("#show-delete").click(function () {
 });
 
 $("#sign-up-button").click(function () {
-   $("#intro-card").toggle();
-   $("#create-account-card").toggle();
+   $("#intro-card").addClass("d-none");
+   $("#create-account-card").removeClass("d-none");
+});
+
+$("#lets-go").click(function () {
+   const email = $("#email-sign-up").val();
+   console.log(`The user inputted: ${email}`);
+
+   // check the length of the email address
+   const emailLength = email.length;
+   console.log(`The length of the email is: ${emailLength}`);
+   if (emailLength === 0) {
+      $("#email-sign-up").addClass("is-invalid");
+      $("#enter-email-error").removeClass("d-none");
+   }
+   const password = $("#password-sign-up").val();
+   console.log(`The user inputted: ${password}`);
+
+   const passwordLength = password.length;
+   console.log(`The length of the password is: ${passwordLength}`);
+
+   if (passwordLength === 0) {
+      $("#password-sign-up").addClass("is-invalid");
+      $("#password-blank-error").removeClass("d-none");
+   } else if (passwordLength < 9 && passwordLength > 0) {
+      $("#password-sign-up").addClass("is-invalid");
+      $("#password-min-char-error").removeClass("d-none");
+   }
 });
