eee6cdd5dd38259ee4d6b11278d0d217c95624c5
2284049 <70497897+2284049@users.noreply.github.com>
2020-09-21 13:53:11 -0700

    add code edits

--- script.js
+++ script.js
@@ -2,135 +2,6 @@ const maxChar = 240;
 $("#save-card").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
 });
-
-$("#create-imagery-input").keyup(function (e) {
-   console.log("Event: ", e);
-
-   // get the text frrom the textarea
-   const text = e.target.value;
-   // console.log("The user inputted: ", text);
-   console.log(`The user inputted: ${text}`);
-
-   // check the length of the text
-   const textLength = text.length;
-   console.log(`The length of the text is: ${textLength}`);
-   if (textLength < 241 && textLength > 0) {
-      $("#save-card").removeAttr("disabled");
-      $("#imagery-characters").removeClass("text-danger");
-      $("#imagery-characters").addClass("text-muted");
-   } else if (textLength > 240) {
-      $("#save-card").attr("disabled", "disabled");
-      $("#imagery-characters").removeClass("text-muted");
-      $("#imagery-characters").addClass("text-danger");
-   } else if (textLength === 0) {
-      $("#save-card").attr("disabled", "disabled");
-      $("#imagery-characters").removeClass("text-danger");
-      $("#imagery-characters").addClass("text-muted");
-   }
-
-   // update the character counter
-   $("#imagery-char-count").html(textLength);
-});
-
-$("#create-answer-input").keyup(function (e) {
-   console.log("Event: ", e);
-
-   // get the text frrom the textarea
-   const text = e.target.value;
-   // console.log("The user inputted: ", text);
-   console.log(`The user inputted: ${text}`);
-
-   // check the length of the text
-   const textLength = text.length;
-   console.log(`The length of the text is: ${textLength}`);
-   if (textLength < 241 && textLength > 0) {
-      $("#next").removeAttr("disabled");
-      $("#answer-characters").removeClass("text-danger");
-      $("#answer-characters").addClass("text-muted");
-   } else if (textLength > 240) {
-      $("#next").attr("disabled", "disabled");
-      $("#answer-characters").removeClass("text-muted");
-      $("#answer-characters").addClass("text-danger");
-   } else if (textLength === 0) {
-      $("#next").attr("disabled", "disabled");
-      $("#answer-characters").removeClass("text-danger");
-      $("#answer-characters").addClass("text-muted");
-   }
-
-   // update the character counter
-   $("#answer-char-count").html(textLength);
-});
-
-$("#edit-imagery-input").keyup(function (e) {
-   console.log("Event: ", e);
-   const text = e.target.value;
-   console.log(`The user inputted: ${text}`);
-   const textLength = text.length;
-   console.log(`The length of the text is: ${textLength}`);
-
-   if (textLength <= 240 && textLength >= 0) {
-      $("#edit-imagery-characters").removeClass("text-danger");
-   } else {
-      $("#edit-imagery-characters").addClass("text-danger");
-   }
-   const imageryInput = $("#edit-imagery-input").val();
-   console.log(`The user inputted: ${imageryInput}`);
-   const imageryInputLength = imageryInput.length;
-   console.log(`The length of the imagery is: ${imageryInputLength}`);
-   const answerInput = $("#edit-answer-input").val();
-   console.log(`The user inputted: ${answerInput}`);
-   const answerInputLength = answerInput.length;
-   console.log(`The length of the answer is: ${answerInputLength}`);
-
-   if (
-      imageryInputLength > 0 &&
-      imageryInputLength <= maxChar &&
-      answerInputLength > 0 &&
-      answerInputLength <= maxChar
-   ) {
-      $("#edit-save").removeAttr("disabled");
-   } else {
-      $("#edit-save").attr("disabled", "disabled");
-   }
-
-   // update the character counter
-   $("#edit-imagery-char-count").html(textLength);
-});
-
-$("#edit-answer-input").keyup(function (e) {
-   console.log("Event: ", e);
-   const text = e.target.value;
-   console.log(`The user inputted: ${text}`);
-   const textLength = text.length;
-   console.log(`The length of the text is: ${textLength}`);
-   if (textLength < 241 && textLength >= 0) {
-      $("#edit-answer-characters").removeClass("text-danger");
-   } else {
-      $("#edit-answer-characters").addClass("text-danger");
-   }
-   const imageryInput = $("#edit-imagery-input").val();
-   console.log(`The user inputted: ${imageryInput}`);
-   const imageryInputLength = imageryInput.length;
-   console.log(`The length of the imagery is: ${imageryInputLength}`);
-   const answerInput = $("#edit-answer-input").val();
-   console.log(`The user inputted: ${answerInput}`);
-   const answerInputLength = answerInput.length;
-   console.log(`The length of the answer is: ${answerInputLength}`);
-
-   if (
-      imageryInputLength > 0 &&
-      imageryInputLength < 241 &&
-      answerInputLength > 0 &&
-      answerInputLength < 241
-   ) {
-      $("#edit-save").removeAttr("disabled");
-   } else {
-      $("#edit-save").attr("disabled", "disabled");
-   }
-   // update the character counter
-   $("#edit-answer-char-count").html(textLength);
-});
-
 $("#back-to-answer").click(function () {
    $("#overlay-error").toggleClass("d-flex d-none");
 });
@@ -144,22 +15,88 @@ $("#sign-up-button").click(function () {
    $("#create-account-card").removeClass("d-none");
 });
 
+$("#create-imagery-input").keyup(function (e) {
+   console.log("Event: ", e);
+   const text = e.target.value;
+   console.log(`The user inputted: ${text}`);
+   if (text.length < 241 && text.length > 0) {
+      $("#save-card").removeAttr("disabled");
+      $("#imagery-characters").removeClass("text-danger");
+      $("#imagery-characters").addClass("text-muted");
+   } else if (text.length > 240) {
+      $("#save-card").attr("disabled", "disabled");
+      $("#imagery-characters").removeClass("text-muted");
+      $("#imagery-characters").addClass("text-danger");
+   } else if (text.length === 0) {
+      $("#save-card").attr("disabled", "disabled");
+      $("#imagery-characters").removeClass("text-danger");
+      $("#imagery-characters").addClass("text-muted");
+   }
+   $("#imagery-char-count").html(text.length);
+});
+
+$("#create-answer-input").keyup(function (e) {
+   console.log("Event: ", e);
+   const text = e.target.value;
+   console.log(`The user inputted: ${text}`);
+   if (text.length < 241 && text.length > 0) {
+      $("#next").removeAttr("disabled");
+      $("#answer-characters").removeClass("text-danger");
+      $("#answer-characters").addClass("text-muted");
+   } else if (text.length > 240) {
+      $("#next").attr("disabled", "disabled");
+      $("#answer-characters").removeClass("text-muted");
+      $("#answer-characters").addClass("text-danger");
+   } else if (text.length === 0) {
+      $("#next").attr("disabled", "disabled");
+      $("#answer-characters").removeClass("text-danger");
+      $("#answer-characters").addClass("text-muted");
+   }
+   $("#answer-char-count").html(text.length);
+});
+
+$("#edit-imagery-input, #edit-answer-input").keyup(function (e) {
+   const imageryInput = $("#edit-imagery-input").val();
+   console.log(`The user inputted: ${imageryInput}`);
+   const answerInput = $("#edit-answer-input").val();
+   console.log(`The user inputted: ${answerInput}`);
+   if (imageryInput.length <= 240 && imageryInput.length >= 0) {
+      $("#edit-imagery-characters").removeClass("text-danger");
+   } else {
+      $("#edit-imagery-characters").addClass("text-danger");
+   }
+   if (answerInput.length < 241 && answerInput.length >= 0) {
+      $("#edit-answer-characters").removeClass("text-danger");
+   } else {
+      $("#edit-answer-characters").addClass("text-danger");
+   }
+
+   if (
+      imageryInput.length > 0 &&
+      imageryInput.length < 241 &&
+      answerInput.length > 0 &&
+      answerInput.length < 241
+   ) {
+      $("#edit-save").removeAttr("disabled");
+   } else {
+      $("#edit-save").attr("disabled", "disabled");
+   }
+   $("#edit-imagery-char-count").html(imageryInput.length);
+   $("#edit-answer-char-count").html(textLength);
+});
+
 $("#lets-go").click(function () {
    const email = $("#email-sign-up").val();
    console.log(`The user inputted: ${email}`);
-   const trimmedEmail = email.trim();
-   const lowerCasedEmail = trimmedEmail.toLowerCase();
+   const trimmedAndLowerCasedEmail = email.trim().toLowerCase();
    console.log(
-      `The user's trimmed and lower cased email address: ${lowerCasedEmail}`
+      `The user's trimmed and lower cased email address: ${trimmedAndLowerCasedEmail}`
    );
 
-   // check the length of the email address
-   const emailLength = email.length;
-   console.log(`The length of the email is: ${emailLength}`);
-   if (emailLength === 0) {
+   if (email.length === 0) {
       $("#email-sign-up").addClass("is-invalid");
       $("#enter-email-error").removeClass("d-none");
-   } else if (emailLength > 0) {
+   } else if (email.length > 0) {
       $("#email-sign-up").removeClass("is-invalid");
       $("#email-sign-up").addClass("is-valid");
       $("#enter-email-error").addClass("d-none");
@@ -168,8 +105,8 @@ $("#lets-go").click(function () {
    // ONE WAY TO SEPARATE LOCAL PART OF EMAIL
    // USING DELIMITER & SLICE METHOD
    // const delimiter = "@";
-   // const indexOfEmailAtDelimiter = lowerCasedEmail.indexOf(delimiter);
-   // const localPartEmail = lowerCasedEmail.slice(0, indexOfEmailAtDelimiter);
+   // const indexOfEmailAtDelimiter = trimmedAndLowerCasedEmail.indexOf(delimiter);
+   // const localPartEmail = trimmedAndLowerCasedEmail.slice(0, indexOfEmailAtDelimiter);
    // console.log(
    //    `This is the user inputted local part of email: ${localPartEmail}`
    // );
@@ -178,10 +115,10 @@ $("#lets-go").click(function () {
 
    // SECOND WAY TO SEPARATE LOCAL PART OF EMAIL
    // USING SPLIT METHOD
-   const emailParts = lowerCasedEmail.split("@");
+   const emailParts = trimmedAndLowerCasedEmail.split("@");
    console.log(`Here is the lower cased email in parts: `, emailParts);
    const localPartEmail = emailParts[0];
-   console.log(`Here is the local part of the email: `localPartEmail)
+   console.log(`Here is the local part of the email: `, localPartEmail);
 
    const password = $("#password-sign-up").val();
    console.log(`The user inputted: ${password}`);
@@ -190,9 +127,6 @@ $("#lets-go").click(function () {
    console.log(
       `The user inputted lower cased password is: ${lowerCasedPassword}`
    );
-
-   const passwordLength = password.length;
-   console.log(`The length of the password is: ${passwordLength}`);
 
    // combining mostInsecurePasswords and secondMostInsecurePasswords
    const allInsecurePasswords = mostInsecurePasswords.concat(
@@ -206,6 +140,7 @@ $("#lets-go").click(function () {
       allFlatInsecurePasswords
    );
 
+   // getting rid of duplicates
    const allUniqueInsecurePasswords = [...new Set(allFlatInsecurePasswords)];
    console.log(
       `Here's a list of all unique insecure passwords:\n `,
@@ -251,9 +186,8 @@ $("#lets-go").click(function () {
       // remove the booleans and keep numbers and strings:
       if (typeof value === "number" || typeof value === "string") {
          // convert numbers to strings:
-         const numAsString = String(value);
          unacceptablePasswordStrings = unacceptablePasswordStrings.concat(
-            numAsString
+            String(value)
          );
       }
    }
@@ -306,11 +240,11 @@ $("#lets-go").click(function () {
       unacceptablePasswords
    );
 
-   if (passwordLength === 0) {
+   if (password.length === 0) {
       $("#password-sign-up").addClass("is-invalid");
       $("#password-error").removeClass("d-none");
       $("#password-error").html("Please create a password.");
-   } else if (passwordLength < 9 && passwordLength > 0) {
+   } else if (password.length < 9 && password.length > 0) {
       $("#password-sign-up").addClass("is-invalid");
       $("#password-error").removeClass("d-none");
       $("#password-error").html("Your password must be at least 9 characters.");
@@ -334,5 +268,25 @@ $("#lets-go").click(function () {
       $("#password-sign-up").addClass("is-valid");
       $("#password-error").addClass("d-none");
       $("#password-error").html("");
+   }
+
+   let createdAt = new Date(Date.now());
+   const year = createdAt.getFullYear();
+   const month = createdAt.getMonth();
+   const day = createdAt.getDate();
+   const hour = createdAt.getHours();
+   const minutes = createdAt.getMinutes();
+   const seconds = createdAt.getSeconds();
+   const milliseconds = createdAt.getMilliseconds();
+
+   const yearPart = String(year);
+   const monthPart = String(month + 1);
+   const dayPart = String(day);
+   if (monthPart.length < 2) {
+      let createdAt = yearPart + "0" + monthPart + dayPart;
+      console.log(`Created at: ${createdAt}`);
+   } else {
+      let createdAt = yearPart + monthPart + dayPart;
+      console.log(`Created at: ${createdAt}`);
    }
 });
